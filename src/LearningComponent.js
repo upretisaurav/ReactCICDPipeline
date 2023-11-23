151fa5dc5a64ef21076b24200ebb7c1d040176aa
Saurav <Upretisaurav7@gmail.com>
2023-11-23 23:58:56 +0545

    testing second time CI pipeline

--- src/LearningComponent.js
+++ src/LearningComponent.js
@@ -3,8 +3,9 @@ import React from 'react';
 function LearningComponent() {
   return (
     <div>
-      <h1>We're learning CI/CD here</h1>
-      <p>This is my first React project</p>
+      <h1>We're learning CI/CD here.</h1>
+      <p>This is my first React project!</p>
+      <p>Learning React so far!</p>
     </div>
   );
 }
--- src/LearningComponent.test.js
+++ src/LearningComponent.test.js
@@ -3,6 +3,7 @@ import LearningComponent from './LearningComponent';
 
 test('renders learning messages', () => {
   render(<LearningComponent />);
-  expect(screen.getByText("We're learning CI/CD here")).toBeInTheDocument();
-  expect(screen.getByText("This is my first React project")).toBeInTheDocument();
+  expect(screen.getByText("We're learning CI/CD here.")).toBeInTheDocument();
+  expect(screen.getByText("This is my first React project!")).toBeInTheDocument();
+  expect(screen.getByText("Learning React so far!")).toBeInTheDocument();
 });
