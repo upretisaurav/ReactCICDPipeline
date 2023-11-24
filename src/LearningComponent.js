14253cb7a8d6648d29434a3b14153a7277f1cea6
Saurav <Upretisaurav7@gmail.com>
2023-11-24 22:12:04 +0545

    some changes in the UI

--- src/LearningComponent.js
+++ src/LearningComponent.js
@@ -3,9 +3,10 @@ import React from 'react';
 function LearningComponent() {
   return (
     <div>
-      <h1>We're learning CI/CD here.</h1>
-      <p>This is my first React project!</p>
-      <p>Loving React so far!</p>
+      <h1>Welcome to My Learning Journey!</h1>
+      <p>I started this project to dive deep into CI/CD and React.</p>
+      <p>Exploring React has been an amazing experience!</p>
+      <p>Every step in this journey adds to my skills and knowledge.</p>
       <p>- Saurav Upreti</p>
     </div>
   );
--- src/LearningComponent.test.js
+++ src/LearningComponent.test.js
@@ -3,8 +3,9 @@ import LearningComponent from './LearningComponent';
 
 test('renders learning messages', () => {
   render(<LearningComponent />);
-  expect(screen.getByText("We're learning CI/CD here.")).toBeInTheDocument();
-  expect(screen.getByText("This is my first React project!")).toBeInTheDocument();
-  expect(screen.getByText("Loving React so far!")).toBeInTheDocument();
+  expect(screen.getByText("Welcome to My Learning Journey!")).toBeInTheDocument();
+  expect(screen.getByText("I started this project to dive deep into CI/CD and React.")).toBeInTheDocument();
+  expect(screen.getByText("Exploring React has been an amazing experience!")).toBeInTheDocument();
+  expect(screen.getByText("Every step in this journey adds to my skills and knowledge.")).toBeInTheDocument();
   expect(screen.getByText("- Saurav Upreti")).toBeInTheDocument();
 });
